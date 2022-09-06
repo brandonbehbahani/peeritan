124639f741f2d16a670a395b2768803773ee7348
Brandon Behbahani <brandonbehbahani@gmail.com>
2022-09-05 23:00:50 -0400

    more peer js stuff
    more peer js stuff

--- peerapp/src/App.svelte
+++ peerapp/src/App.svelte
@@ -6,7 +6,21 @@
   console.log("initializing connnection to peers.");
   var peer =  new Peer();
 
-  
+  peer = new Peer(null, { debug: 2 });
+
+  peer.on('open', function (id) {
+                        // Workaround for peer.reconnect deleting previous id
+                        if (peer.id === null) {
+                            console.log('Received null id from peer open');
+                            peer.id = lastPeerId;
+                        } else {
+                            lastPeerId = peer.id;
+                        }
+
+                        console.log('ID: ' + peer.id);
+                    });
+
+
 
 </script>
 
